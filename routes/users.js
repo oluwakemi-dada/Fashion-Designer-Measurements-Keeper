4dcd044d521ddb7e3b5a33283e60d44a129cbf4d
Dada Oluwakemi <dadaoluwakemi97@gmail.com>
2021-03-04 14:02:19 +0100

    User Model and Validation

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 // @route      POST  api/users
 // @desc       Register a user
 // @access     Public
-router.post('/', (req, res) => {
-  res.send('Register a user');
-});
+router.post(
+  '/',
+  [
+    body('name', 'Please add name').not().isEmpty(),
+    body('email', 'Please include a valid email').isEmail(),
+    body(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    res.send('Passed')
+  }
+);
 
 module.exports = router;
